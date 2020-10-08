6ea50972fc2ef8647e5e48266614293e1c0c901b
EmanuelC16-bit <66982258+EmanuelC16-bit@users.noreply.github.com>
2020-10-08 16:27:53 -0500

    Update editarPerfil.php

--- views/editarPerfil.php
+++ views/editarPerfil.php
@@ -3,7 +3,7 @@
 <head>
     <script src="https://code.jquery.com/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/styleAgregarAlEdPer.css">
+    <link rel="stylesheet" href="Styles/styleAgregarAlEdPer.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
