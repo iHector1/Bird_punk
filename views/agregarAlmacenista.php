4383cef7a2bab7edc625127fb544ac9fd7f9df9f
EmanuelC16-bit <66982258+EmanuelC16-bit@users.noreply.github.com>
2020-10-07 20:41:04 -0500

    Add files via upload

---
+++ views/agregarAlmacenista.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="Styles/styleAgregarAlEdPer.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+<div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+        <h4 class="text-white">Ejemplo</h4>
+        <span class="text-muted">Aqui pueden ir botones</span>
+        </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+     </nav>
+</div>
+
+<header>
+    <h2>Bird Punk</h2>
+    <img src="Imagenes/punk_bird.png" width="150px" height="150px" alt="profile picture">
+</header>
+
+<body>
+    <div class="contenedorAlmacenista">
+        <table>
+            <tr>
+                <td>
+                    <P>Nombre Almacenista <br> Domicilio (calle1,numero ext #11, numero int #12, colonia)<br> almacenista1@gmail.com <br> </P>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-default btn-circle"><i class="fa fa-check"></i>X</button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <P>Nombre Almacenista <br> Domicilio (calle1,numero ext #11, numero int #12, colonia)<br> almacenista1@gmail.com <br> </P>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-default btn-circle"><i class="fa fa-check"></i>X</button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <P>Nombre Almacenista <br> Domicilio (calle1,numero ext #11, numero int #12, colonia)<br> almacenista1@gmail.com <br> </P>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-default btn-circle"><i class="fa fa-check"></i>X</button>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <P>Nombre Almacenista <br> Domicilio (calle1,numero ext #11, numero int #12, colonia)<br> almacenista1@gmail.com <br> </P>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-default btn-circle"><i class="fa fa-check"></i>X</button>
+                </td>
+            </tr>
+        </table>
+        <button type="button" class="btn btn-light">Agregar Almacenista</button>
+    </div>
+</body>
+
+</html>
