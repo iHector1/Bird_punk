55155ef4943091401b3c15784e035d16cb71f098
Priska-cmd <72408726+Priska-cmd@users.noreply.github.com>
2020-10-13 21:00:24 -0500

    Navbar

--- views/cancelarPedido.php
+++ views/cancelarPedido.php
@@ -13,6 +13,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="Styles/IndexStyle.css" />
+    <link rel="stylesheet" href="Styles/styleVerProducto.css">
 </head>
 <body>
     <!--Barra de navegación-->
@@ -23,15 +24,15 @@
             <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item ">
-                        <a class="nav-link border-right" href="verProducto.php">HOMBRES</a>
+                        <a class="nav-link border-right" href="#">HOMBRES</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="verProducto.php">MUJERES</a>
+                        <a class="nav-link" href="#">MUJERES</a>
                     </li>
                 </ul>
             </div>
             <div class="navbar w-100 order-2  mx-auto">
-                <a class="navbar-brand mx-auto" href="#"><img class="mx-auto" src="imagenes/logo.PNG" width="60%"></a>
+                <img class="mx-auto" src="imagenes/logo.PNG" width="60%">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
                     <i class="fa fa-bars" aria-hidden="true"></i>
                 </button>
@@ -50,13 +51,46 @@
         <div class="navbar navbar-expand-md navbar-light"> </div>
 
     </section>
-    <!-----------Banner------------>
-    <section id="banner">
-        <div class="fadeInDown"> 
-            <img class="rounded mx-auto d-block" src="imagenes/banner-bg.jpg" width="100%" padding-top="2px">
+
+    <h4>Numero de pedido: 123DER2345</h4>
+    <!-------articulo------->
+    <div class="art">
+        <div>
+            <div class="producto">
+                <img src="tenis.jpg" class="cancelar">
+            </div>    
+            <div class="producto">
+                <h6>Nombre articulo</h6>
+                <h6>Cantidad</h6>
+                <h6>Talla</h6>
+                <h6>Precio1</h6>
+            </div>
         </div>
+        <div>
+            <div class="producto">
+                <img src="tenis.jpg" class="cancelar">
+            </div>    
+            <div class="producto">
+                <h6>Nombre articulo</h6>
+                <h6>Cantidad</h6>
+                <h6>Talla</h6>
+                <h6>Precio2</h6>
+            </div>
+        </div>
+            <div class="producto">
+                <img src="tenis.jpg" class="cancelar">
+            </div>    
+            <div class="producto">
+                <h6>Nombre articulo</h6>
+                <h6>Cantidad</h6>
+                <h6>Talla</h6>
+                <h6>Precio3</h6>
+            </div>
+    </div> 
+    <input type="submit" class="fadeIn fourth" value="Cancelar pedido">
+    <input type="submit" class="fadeIn fourt" value="Agregar producto">
+    <input type="submit" class="fadeIn four" value="Finalizar compra">    
         
-    </section>
     <!---------FOOTER--------->
     <section id="footer">
 
