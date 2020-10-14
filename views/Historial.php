d60e6a581eccf09cbd744cc18eade37c6e9d0c25
Priska-cmd <72408726+Priska-cmd@users.noreply.github.com>
2020-10-13 20:13:09 -0500

    Revert "daskdskjadhjaskdkas"
    This reverts commit 40581a2e5ab211e1baad67703c158ff10b1786b9.

--- views/Historial.php
+++ views/Historial.php
@@ -26,15 +26,15 @@
             <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item ">
-                        <a class="nav-link border-right" href="verProducto.html">HOMBRES</a>
+                        <a class="nav-link border-right" href="#">HOMBRES</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="verProducto.html">MUJERES</a>
+                        <a class="nav-link" href="#">MUJERES</a>
                     </li>
                 </ul>
             </div>
             <div class="navbar w-100 order-2  mx-auto">
-                <a href="Index.html"><img class="mx-auto" src="imagenes/logo.PNG" width="60%"></a>
+                <img class="mx-auto" src="imagenes/logo.PNG" width="60%">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
                     <i class="fa fa-bars" aria-hidden="true"></i>
                 </button>
