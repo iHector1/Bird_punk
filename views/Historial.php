efa78e3aa45d877603bc2484cc0a87d35e107c35
luisbravor <64480392+luisbravor@users.noreply.github.com>
2020-10-27 10:39:28 -0600

    Final con fecha de compra historial

--- views/Historial.php
+++ views/Historial.php
@@ -45,15 +45,15 @@
             <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item ">
-                        <a class="nav-link border-right" href="#">HOMBRES</a>
+                        <a class="nav-link border-right" href="verProductoBoys.php">HOMBRES</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">MUJERES</a>
+                        <a class="nav-link" href="verProductoGirls.php">MUJERES</a>
                     </li>
                 </ul>
             </div>
             <div class="navbar w-100 order-2  mx-auto">
-                <img class="mx-auto" src="imagenes/logo.PNG" width="60%">
+                <a href="Index.php"><img src="imagenes/logo.PNG" width="60%" style="margin-left:150px;"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
                     <i class="fa fa-bars" aria-hidden="true"></i>
                 </button>
@@ -98,7 +98,7 @@
                                 while($row = sqlsrv_fetch_array($stmt)) 
                                 {
                                     $noorden = $row[0];
-                                    $f = $row[1];
+                                    $fecha = $row[1]->format('Y-m-d');
                                     $total = $row[2]; 
                                     $estatus = $row[3];
                                 ?>
