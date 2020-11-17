aba5a5e49e53dbd83363381b4abd1d8ba8e81a7a
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-11-16 19:46:04 -0600

    agregar articulo 100

--- views/Backend/agregarArticulo.php
+++ views/Backend/agregarArticulo.php
@@ -1,47 +1,15 @@
 <?php
     
-    include '../conexion.php';
     $nombremodelo = $_POST['nombre'];
     $id_marca = $_POST['marca'];
     $id_talla = $_POST['talla'];
     $genero = $_POST['genero'];
     $precio = $_POST['precio'];
     $stock = $_POST['stock'];
-    //$imagen = $_POST['imagen'];
-
-    $param = array($nombremodelo);
-    $id = sqlsrv_query($conn, "INSERT INTO modelo (Modelo) VALUES (?)", $param);
-    sqlsrv_free_stmt($id); 
-
-    $sql = "SELECT * FROM modelo WHERE Modelo = '".$nombremodelo."'"; 
-    $buscarID = sqlsrv_query($conn,$sql);
-
-    while( $row = sqlsrv_fetch_array($buscarID, SQLSRV_FETCH_NUMERIC)) {
-        $ID_Modelo = (int)"$row[0]";
+    $control=$_GET['control'];
+    if($control!=1){
+        header("Location: http://25.61.144.153/distribuidos/Bird_punk/views/BackendP/BEPagregarArticulo.php?marca=".$id_marca."&talla=".$id_talla."&genero=".$genero."&precio=".$precio."&nombre=".$nombremodelo."&stock=".$stock."&control=3");
+    }else{
+        header("Location: http://25.61.144.153/distribuidos/Bird_punk/views/Backend/AnadirProducto.php");
     }
-    echo $ID_Modelo;
-
-    $nombreIMG = $nombremodelo . "" . $ID_Modelo;
-
-    $nombreIMGInsert = $nombremodelo . "" . $ID_Modelo. ".jpg";
-
-    $param2 = array($ID_Modelo, $id_marca, $id_talla, $genero, $precio, $stock,$nombreIMGInsert);
-    $agregar = sqlsrv_query($conn, "INSERT INTO articulo (ID_Modelo, ID_Marca, ID_Talla, Genero, Precio, Stock, Imagen) VALUES (?,?,?,?,?,?,?)", $param2);
-
-    if($agregar)
-        echo "Artículo agregado correctamente. <br>";
-    else
-        echo "Error, no se pudo agregar el artículo. <br>";
-
-    $file = addslashes($_FILES['browse']['tmp_name']);
-    $filename = addslashes($_FILES['browse']['name']);
-    $file = file_get_contents($file);
-    $file = base64_encode($file);
-    move_uploaded_file($_FILES['browse']['tmp_name'], '../Imagenes/'.$nombreIMG.'.jpg');
-    echo '<script type="text/javascript">
-                alert("Articulo añadido correctamente.")
-                window.location = " http://localhost/Bird_punk/views/AnadirProducto.php"
-                </script>';
-
-    sqlsrv_close($conn);
 ?>
---
+++ views/ServerBD/BDagregarArticulo.php
@@ -0,0 +1,48 @@
+<?php
+    
+    include '../conexion.php';
+    $nombremodelo = $_GET['nombre'];
+    $id_marca = $_GET['marca'];
+    $id_talla = $_GET['talla'];
+    $genero = $_GET['genero'];
+    $precio = $_GET['precio'];
+    $stock = $_GET['stock'];
+    //$imagen = $_GET['imagen'];
+
+    $param = array($nombremodelo);
+    $id = sqlsrv_query($conn, "INSERT INTO modelo (Modelo) VALUES (?)", $param);
+    sqlsrv_free_stmt($id); 
+
+    $sql = "SELECT * FROM modelo WHERE Modelo = '".$nombremodelo."'"; 
+    $buscarID = sqlsrv_query($conn,$sql);
+
+    while( $row = sqlsrv_fetch_array($buscarID, SQLSRV_FETCH_NUMERIC)) {
+        $ID_Modelo = (int)"$row[0]";
+    }
+    echo $ID_Modelo;
+
+    $nombreIMG = $nombremodelo . "" . $ID_Modelo;
+
+    $nombreIMGInsert = $nombremodelo . "" . $ID_Modelo. ".jpg";
+
+    $param2 = array($ID_Modelo, $id_marca, $id_talla, $genero, $precio, $stock,$nombreIMGInsert);
+    $agregar = sqlsrv_query($conn, "INSERT INTO articulo (ID_Modelo, ID_Marca, ID_Talla, Genero, Precio, Stock, Imagen) VALUES (?,?,?,?,?,?,?)", $param2);
+
+    if($agregar)
+        echo "Artículo agregado correctamente. <br>";
+    else
+        echo "Error, no se pudo agregar el artículo. <br>";
+
+    $file = addslashes($_FILES['browse']['tmp_name']);
+    $filename = addslashes($_FILES['browse']['name']);
+    $file = file_get_contents($file);
+    $file = base64_encode($file);
+    move_uploaded_file($_FILES['browse']['tmp_name'], '../Imagenes/'.$nombreIMG.'.jpg');
+    echo '<script type="text/javascript">
+                alert("Articulo añadido correctamente.")
+                window.location = " http://localhost/Bird_punk/views/AnadirProducto.php"
+                </script>';
+
+    sqlsrv_close($conn);
+    header("Location: http://25.61.144.153/distribuidos/Bird_punk/views/BackendP/BEPPagregarArticulos.php?&control=1");
+?>
