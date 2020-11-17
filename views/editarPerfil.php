f4a5cb73ccc4a287a5cc7beac796f041c575b737
luisbravor <64480392+luisbravor@users.noreply.github.com>
2020-11-16 21:17:52 -0600

    fdghfgh

--- views/editarPerfil.php
+++ views/editarPerfil.php
@@ -15,7 +15,8 @@
     }
     $control=$_GET['control'];
     $id=$_GET['id'];
-   if($control!=1){    
+
+    if($control!=1){    
         header('Location:http://25.61.144.153/distribuidos/Bird_punk/views/Backend/infoUsuarioA.php?id='.$id); 
     }
     $datos=unserialize($_GET['datos']);
