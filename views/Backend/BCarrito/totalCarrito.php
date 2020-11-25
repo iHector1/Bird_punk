89bf82b25b11976bfe0b9fd2bec4457ef4990ff8
luisbravor <64480392+luisbravor@users.noreply.github.com>
2020-11-24 21:10:29 -0600

    summary errores

--- views/Backend/BCarrito/totalCarrito.php
+++ views/Backend/BCarrito/totalCarrito.php
@@ -4,6 +4,7 @@
     $total = $_GET['total'];
     $control = $_GET['control'];
     $idu = $_GET['idu'];
+    
     if($control != 1)
     {
         header("Location: http://25.90.201.164/distribuidos/Bird_punk/views/BackendP/BEPtotalCarrito.php?id=".$id_c."&total=$total&idu=$idu");
