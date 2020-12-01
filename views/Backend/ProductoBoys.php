063c91483803924db1ea2ef90928130197dc8fc1
SofiaLanda <54426230+SofiaLanda@users.noreply.github.com>
2020-11-30 19:25:23 -0600

    Hello

--- views/Backend/ProductoBoys.php
+++ views/Backend/ProductoBoys.php
@@ -2,13 +2,14 @@
     $control = $_GET['control'];
     $arreglo = $_GET['datos'];
     $id = $_GET['idu'];
+    $idc = $_GET['idc'
     
     if($control != 1)
     {
-        header("Location: http://25.90.201.164/distribuidos/Bird_punk/views/BackendP/BEPProductoBoys.php?idu=".$id);
+        header("Location: http://25.90.201.164/distribuidos/Bird_punk/views/BackendP/BEPProductoBoys.php?idu=".$id.$idc);
     }else
     {
-        header("Location: http://localhost/Bird_punk/views/verProductoBoys.php?datos=".$arreglo."&control=1&idu=$id");
+        header("Location: http://localhost/Bird_punk/views/verProductoBoys.php?datos=".$arreglo."&control=1&idu=$id.$idc");
     }
 
 ?>
--- views/Backend/ProductoGirls.php
+++ views/Backend/ProductoGirls.php
@@ -1,13 +1,14 @@
 <?php
     $control = $_GET['control'];
     $arreglo = $_GET['datos'];
+    $idc = $_GET['idc'];
     $id = $_GET['idu'];
     if($control != 1)
     {
-        header("Location: http://25.90.201.164/distribuidos/Bird_punk/views/BackendP/BEPProductoGirls.php?idu=".$id);
+        header("Location: http://25.90.201.164/distribuidos/Bird_punk/views/BackendP/BEPProductoGirls.php?idu=".$id.$idc);
     }else
     {
-        header("Location: http://localhost/Bird_punk/views/verProductoGirls.php?datos=".$arreglo."&control=1&idu=$id");
+        header("Location: http://localhost/Bird_punk/views/verProductoGirls.php?datos=".$arreglo."&control=1&idu=$id.$idc");
     }
 
 ?>
