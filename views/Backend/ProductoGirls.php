e765de31e4c9cce14f13900a9e40c42aef0b2400
SofiaLanda <54426230+SofiaLanda@users.noreply.github.com>
2020-11-30 19:31:43 -0600

    Update ProductoGirls.php

--- views/Backend/ProductoGirls.php
+++ views/Backend/ProductoGirls.php
@@ -5,10 +5,10 @@
     $id = $_GET['idu'];
     if($control != 1)
     {
-        header("Location: http://25.90.201.164/distribuidos/Bird_punk/views/BackendP/BEPProductoGirls.php?idu=".$id."idc=".$idc);
+        header("Location: http://25.90.201.164/distribuidos/Bird_punk/views/BackendP/BEPProductoGirls.php?idu=".$id."&idc=".$idc);
     }else
     {
-        header("Location: http://localhost/Bird_punk/views/verProductoGirls.php?datos=".$arreglo."&control=1&idu=$id"."idc=$idc");
+        header("Location: http://localhost/Bird_punk/views/verProductoGirls.php?datos=".$arreglo."&control=1&idu=$id"."&idc=$idc");
     }
 
 ?>
